60f9946310390d84ae94d7b9502b20eda9d1da8d
Anonymous1231234534235423423 <143645138+Ducklord42@users.noreply.github.com>
2023-11-10 17:54:58 -0500

    Create cmmd.sh

---
+++ cmmd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+alias info="echo 'Directory:';pwd;echo '';ls -l"
+#alias files="bash /Users/admin/Desktop/files.sh"
+alias newalias="vim ~/.bashrc;source ~/.bash_profile"
+#alias edit="bash /Users/admin/edit.sh"
+alias discord="open https://discord.com/login"
+alias google="open https://google.com"
+alias youtube="open https://youtube.com"
+#alias music="bash /home/admin-jh/Desktop/comm/music.sh"
+alias commands="cat ~/.bashrc"
+#alias calc="python3 /Users/admin/Desktop/comm/calc.py"
+#alias homework="bash /Users/admin/Desktop/comm/home.sh"
+#alias tmuxs="echo 'Use cntrl+b'; read a; tmux"
