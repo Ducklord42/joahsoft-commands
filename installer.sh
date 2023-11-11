dfc857d85bf5f3ff99b3a4ef33d2784e5d8f8529
Anonymous1231234534235423423 <143645138+Ducklord42@users.noreply.github.com>
2023-11-11 16:49:20 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+echo "MacOS or linux? [m/l]"
+read os
+ab="~/.bash_profile"
+if [ "$os" = "l" ]; then
+	ab="~/.bashrc"
+fi
 echo "Do you have git? [y/n]"
 read con
 if [ "$con" = "y" ]
 then
-	echo "Are you sure you want to download the commands it will make changes to ~/.bashrc? [y/n]"
+	echo "Are you sure you want to download the commands it will make changes to $ab ? [y/n]"
 	read conn
 	if [ "$conn" = "y" ];then
-		echo "Adding commands to ~/.bashrc"
-		touch ~/.cmmd
-		echo "git clone https://github.com/Ducklord42/joahsoft-commands.git" >> ~/.bashrc
-		echo "cd joahsoft-commands/" >> ~/.bashrc
-		echo "cp cmmds.sh ~/.cmmds" >> ~/.bashrc
-		echo "cd ..; rm -fr joahsoft-commands/" >> ~/.bashrc
-  		echo "source ~/.cmmds" >> ~/.bashrc
-    		echo "close this terminal window and create another one for commands to activate"
+		if [ "$os" = "l" ]; then
+			echo "Adding commands to ~/.bashrc"
+			touch ~/.cmmd
+			echo "git clone https://github.com/Ducklord42/joahsoft-commands.git" >> ~/.bashrc
+			echo "cd joahsoft-commands/" >> ~/.bashrc
+			echo "cp cmmds.sh ~/.cmmds" >> ~/.bashrc
+			echo "cd ..; rm -fr joahsoft-commands/" >> ~/.bashrc
+	  		echo "source ~/.cmmds" >> ~/.bashrc
+			echo "close this terminal window and create another one for commands to activate"
+		else
+			echo "Adding commands to ~/.bash_profile"
+			touch ~/.cmmd
+			echo "git clone https://github.com/Ducklord42/joahsoft-commands.git" >> ~/.bash_profile
+			echo "cd joahsoft-commands/" >> ~/.bash_profile
+			echo "cp cmmds.sh ~/.cmmds" >> ~/.bash_profile
+			echo "cd ..; rm -fr joahsoft-commands/" >> ~/.bash_profile
+			echo "source ~/.cmmds" >> ~/.bash_profile
+			echo "close this terminal window and create another one for commands to activate"
+		fi
 	else
-		echo "ok this will probably never change, bye"
+		echo "This software requires changes to the ~/.bashrc"
 	fi
 else
 	echo "Download git to use these commands"
