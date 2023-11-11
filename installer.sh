72dcbb6aea2ccc3ed289127d5a06db09b55cd840
Anonymous1231234534235423423 <143645138+Ducklord42@users.noreply.github.com>
2023-11-10 19:35:34 -0500

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Do you have git? [y/n]"
+read con
+if [ "$con" = "y" ]
+then
+	echo "Are you sure you want to download the commands it will make changes to ~/.bashrc? [y/n]"
+	read conn
+	if [ "$conn" = "y" ];then
+		echo "Adding commands to ~/.bashrc"
+		touch ~/cmmd.sh
+		echo "git clone https://github.com/Ducklord42/joahsoft-commands.git" >> ~/.bashrc
+		echo "cd joahsoft-commands/" >> ~/.bashrc
+		echo "cp cmmds.sh ~/cmmds.sh" >> ~/.bashrc
+		echo "cd ..; rm -fr joahsoft-commands/; source ~/cmmds.sh;" >> ~/.bashrc
+	else
+		echo "ok this will probably never change, bye"
+	fi
+else
+	echo "Download git to use these commands"
+fi
